c394314735326ec7ff21388792ea222478d75188
Claudio Gonçalves <claudiogoncalves@Mayaras-MBP.lan>
2025-02-22 13:24:32 +0000

    Create invite page and components form

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,24 +1,18 @@
 import type { ComponentProps, ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface IconButtonProps extends ComponentProps<'button'> {
   children: ReactNode
 }
 
-export function IconButton(props: IconButtonProps) {
+export function IconButton({ className, ...props }: IconButtonProps) {
   return (
     <button
       {...props}
-      className="
-        p-1.5 
-        bg-gray-500 
-        text-blue 
-        rounded-md 
-        transition-colors 
-        duration-300 
-        cursor-pointer 
-        hover:bg-blue 
-        hover:text-gray-900
-      "
+      className={twMerge(
+        'p-1.5 bg-gray-500 text-blue rounded-md transition-colors duration-300 cursor-pointer hover:bg-blue hover:text-gray-900',
+        className
+      )}
     />
   )
 }
